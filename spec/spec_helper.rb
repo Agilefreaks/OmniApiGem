1fdd2c92276bc405bae5c6ac4c37f0ef171054fa
Cristi Badila <cristi.badila@gmail.com>
2015-09-18 16:41:03 +0300

    fixes issue user factory which was not properly refactored

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'bundler/setup'
 Bundler.setup
 
 require 'rspec/its'
-require 'OmniApi'
+require 'active_resource'
+require 'active_support/json'
+require 'omni_api'
 OmniApi.config.base_url = 'http://localhost:9292/api/v1'
 OmniApi.config.client_access_token = 'random'
